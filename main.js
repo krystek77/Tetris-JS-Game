e4b85bf80eca9ae7477971114489074abc5b1faf
krystek77 <krystian.wrona77@gmail.com>
2019-09-06 08:40:34 +0200

    add JSDoc to drawSquare function

--- main.js
+++ main.js
@@ -10,11 +10,16 @@ canvas.height = ROWS * SIZE_SQUARE;
 context.fillStyle = 'black';
 context.fillRect(0, 0, canvas.width, canvas.height);
 
+/**
+ * Draw square at position x and y with given fill color
+ *
+ * @param {Number} x
+ * @param {Number} y
+ * @param {String} color
+ */
 function drawSquare(x, y, color) {
 	context.fillStyle = color;
 	context.strokeStyle = 'white';
 	context.fillRect(x * SIZE_SQUARE, y * SIZE_SQUARE, SIZE_SQUARE, SIZE_SQUARE);
 	context.strokeRect(x * SIZE_SQUARE, y * SIZE_SQUARE, SIZE_SQUARE, SIZE_SQUARE);
 }
-
-
