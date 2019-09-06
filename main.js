db69645583c43566eb3a3db6e59280e5e92ce1f4
krystek77 <krystian.wrona77@gmail.com>
2019-09-06 08:39:03 +0200

    add drawSquare function

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
-canvas.width = 10 * 20;
-canvas.height = 20 * 20;
+const ROWS = 20;
+const COLUMNS = 10;
+const SIZE_SQUARE = 20;
+
+canvas.width = COLUMNS * SIZE_SQUARE;
+canvas.height = ROWS * SIZE_SQUARE;
 
 context.fillStyle = 'black';
 context.fillRect(0, 0, canvas.width, canvas.height);
 
-context.strokeStyle = 'white';
-context.strokeRect(0, 0, canvas.width, canvas.height);
+function drawSquare(x, y, color) {
+	context.fillStyle = color;
+	context.strokeStyle = 'white';
+	context.fillRect(x * SIZE_SQUARE, y * SIZE_SQUARE, SIZE_SQUARE, SIZE_SQUARE);
+	context.strokeRect(x * SIZE_SQUARE, y * SIZE_SQUARE, SIZE_SQUARE, SIZE_SQUARE);
+}
+
+
