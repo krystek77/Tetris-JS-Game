a4fcf6a80787abf23fa0f52c4f45b34fe67281b8
krystek77 <krystian.wrona77@gmail.com>
2019-09-09 08:14:15 +0200

    add row counts to player object

--- main.js
+++ main.js
@@ -134,6 +134,7 @@ const player = {
 	life: 3,
 	score: 0,
 	level: 1,
+	rowCount: 0,
 };
 /**
  * Drop player
@@ -308,10 +309,10 @@ function draw() {
 
 	drawText('SCORE', 60, 20, 'white', 'bold 20px sans-serif', 40);
 	drawText(player.score, 60, 50, 'green', 'bold 40px sans-serif', 40);
-	drawText('LIFES', 60, (canvas.height-40), 'orange', 'bold 20px sans-serif', 40);
-	drawText("00"+player.life, 60, (canvas.height-10), 'white', 'bold 40px sans-serif', 40);
-	drawText('LEVEL', (canvas.width - 100), (canvas.height-40), 'orange', 'bold 20px sans-serif', 40);
-	drawText("0"+player.level, (canvas.width - 100), (canvas.height-10), 'white', 'bold 40px sans-serif', 40);
+	drawText('LIFES', 60, canvas.height - 40, 'orange', 'bold 20px sans-serif', 40);
+	drawText('00' + player.life, 60, canvas.height - 10, 'white', 'bold 40px sans-serif', 40);
+	drawText('LEVEL', canvas.width - 100, canvas.height - 40, 'orange', 'bold 20px sans-serif', 40);
+	drawText('0' + player.level, canvas.width - 100, canvas.height - 10, 'white', 'bold 40px sans-serif', 40);
 
 	drawArena(arena, { x: 0, y: 0 });
 	drawMatrix(player.matrix, player.position);
