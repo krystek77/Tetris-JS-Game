38f8465bb8e156dc1885db4f1ef29070e9ee856a
krystek77 <krystian.wrona77@gmail.com>
2019-09-09 17:10:28 +0200

    remove logs to console

--- main.js
+++ main.js
@@ -146,8 +146,7 @@ const player = {
  */
 function playerDrop() {
 	player.position.y++;
-	if (collide(arena, player)) {
-		// console.log('collide');
+	if (collide(arena, player)) {;
 		player.position.y--;
 		merge(arena, player);
 		playerReset();
@@ -164,12 +163,10 @@ function checkFullRow() {
 	let rowCounter = 1;
 	for (let y = arena.length - 1; y > 0; y--) {
 		let isFullRow = true;
-		// console.log(arena[y]);
 		for (let x = 0; x < arena[y].length; x++) {
 			isFullRow = isFullRow && arena[y][x] != 0;
 		}
 		if (isFullRow) {
-			// console.log('REMOVE ROW AND ADD AT START');
 			const row = arena.splice(y, 1)[0].fill(0);
 			arena.unshift(row);
 			player.score += rowCounter * 10;
@@ -210,12 +207,9 @@ function playerReset() {
 	player.position.x = Math.floor(arena[0].length / 2 - player.matrix[0].length / 2);
 	if (collide(arena, player)) {
 		player.life--;
-		console.log(player.life);
 		arena.forEach(row => {
 			row.fill(0);
 		});
-
-		// console.table(arena);
 	}
 }
 
@@ -250,29 +244,19 @@ function rotateMatrix(matrix, direction = 1) {
 function playerRotate(direction) {
 	let offset = 1;
 	player.matrix = rotateMatrix(player.matrix, direction);
-	// if (collide(arena, player)) {
-	// 	console.log('collision at rotation');
-	// 	if (player.position.x < arena[0].length / 2) {
-	// 		console.log('collision at left edge');
-	// 		player.position.x += offset;
-	// 	} else if (player.position.x > arena[0].length / 2) {
-	// 		console.log('collision at right edge');
-	// 		player.position.x -= offset;
-	// 	}
-	// }
+
 	while (collide(arena, player)) {
-		console.log('collide');
+		
 		let pos = player.position.x;
-		console.log('pos', pos);
+		
 		player.position.x += offset;
 		offset = -(offset + (offset > 0 ? 1 : -1));
 		if (offset > player.matrix[0].length) {
-			console.log('inner');
+			
 			player.position.x = pos;
 			player.matrix = rotateMatrix(player.matrix, -direction);
 			return;
 		}
-		console.log(offset);
 	}
 }
 
@@ -380,7 +364,6 @@ function resetGame(event) {
 	const posX = event.offsetX;
 	const posY = event.offsetY;
 	if (posX > canvas.width - 90 && posX < canvas.width - 90 + 80 && posY > 10 && posY < 10 + 40) {
-		// console.log(posX, posY);
 		playerReset();
 		player.life = 3;
 		player.score = 0;
@@ -394,19 +377,14 @@ function resetGame(event) {
 
 function control(event) {
 	if (event.key === 'ArrowLeft') {
-		// console.log('MOVE LEFT');
 		playerMove(-1);
 	} else if (event.key === 'ArrowRight') {
-		// console.log('MOVE RIGHT');
 		playerMove(+1);
 	} else if (event.key === 'ArrowUp') {
-		// console.log('ROTATE CLOCKWISE');
 		playerRotate(1);
 	} else if (event.key === 'ArrowDown') {
-		// console.log('ROTATE COUNTER CLOCKWISE');
 		playerRotate(-1);
 	} else if (event.key === 'd') {
-		// console.log('DROP');
 		playerDrop();
 	}
 }
